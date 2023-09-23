ab4e1b55e4c696b01c347897cf13f6df60f5496b
BeluTrac <tracchiabelen@gmail.com>
2023-09-23 23:10:40 +0200

    Add documents, dao and services

---
+++ src/main/java/com/example/springbootwebfluxapirest/models/dao/CategoriaDao.java
@@ -0,0 +1,8 @@
+package com.example.springbootwebfluxapirest.models.dao;
+
+import com.example.springbootwebfluxapirest.models.documents.Categoria;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+public interface CategoriaDao extends ReactiveMongoRepository<Categoria, String> {
+
+}
---
+++ src/main/java/com/example/springbootwebfluxapirest/models/dao/ProductoDao.java
@@ -0,0 +1,8 @@
+package com.example.springbootwebfluxapirest.models.dao;
+
+import com.example.springbootwebfluxapirest.models.documents.Producto;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+public interface ProductoDao extends ReactiveMongoRepository<Producto, String> {
+
+}
