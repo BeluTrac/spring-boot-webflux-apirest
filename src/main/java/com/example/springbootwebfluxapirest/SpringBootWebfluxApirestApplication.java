4a111fdbfe7f4a01208c1121dd6e0c4ade8dfe46
BeluTrac <tracchiabelen@gmail.com>
2023-09-23 22:44:34 +0200

    Initial commit

---
+++ src/main/java/com/example/springbootwebfluxapirest/SpringBootWebfluxApirestApplication.java
@@ -0,0 +1,27 @@
+package com.example.springbootwebfluxapirest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+
+@SpringBootApplication
+public class SpringBootWebfluxApirestApplication implements CommandLineRunner {
+
+	@Autowired
+	private ReactiveMongoTemplate mongoTemplate;
+
+	private static final Logger log = LoggerFactory.getLogger(SpringBootWebfluxApirestApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebfluxApirestApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+}
