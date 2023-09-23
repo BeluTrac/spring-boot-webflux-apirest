9ef6fa792cb8fcfd8d76ebabd58dace4b270ef49
BeluTrac <tracchiabelen@gmail.com>
2023-09-24 00:58:43 +0200

    Add post with upload photo

--- src/main/java/com/example/springbootwebfluxapirest/controllers/ProductoController.java
+++ src/main/java/com/example/springbootwebfluxapirest/controllers/ProductoController.java
@@ -2,24 +2,44 @@ package com.example.springbootwebfluxapirest.controllers;
 
 import com.example.springbootwebfluxapirest.models.documents.Producto;
 import com.example.springbootwebfluxapirest.models.services.ProductoService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.io.File;
 import java.net.URI;
 import java.util.Date;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/productos")
 public class ProductoController {
 
+    @Value("${config.uploads.path}")
+    private String path;
     private final ProductoService service;
 
     public ProductoController(ProductoService service) {
         this.service = service;
+    }
+
+    @PostMapping("/upload/{id}")
+    public Mono<ResponseEntity<Producto>> uploadPhoto(@PathVariable String id, @RequestPart FilePart file) {
+        return service.findById(id).flatMap(p -> {
+                    p.setFoto(UUID.randomUUID().toString() + "-" + replaceInvalidCharacters(file.filename()));
+                    return file.transferTo(new File(path + p.getFoto()))
+                            .then(service.save(p));
+                }).map(p -> ResponseEntity.ok().body(p))
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    private String replaceInvalidCharacters(String string){
+        return string.replaceAll("[:\\\\ ]", "");
     }
 
     @GetMapping
@@ -44,6 +64,20 @@ public class ProductoController {
                 .created(URI.create("api/productos/".concat(p.getId())))
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(p));
+    }
+
+    @PostMapping("/v2")
+    public Mono<ResponseEntity<Producto>> createProductWithPhoto(Producto producto, @RequestPart FilePart file) {
+        if (producto.getCreateAt() == null) {
+            producto.setCreateAt(new Date());
+        }
+        producto.setFoto(UUID.randomUUID() + "-" + replaceInvalidCharacters(file.filename()));
+        return file.transferTo(new File(path + producto.getFoto()))
+                .then(service.save(producto))
+                .map(p -> ResponseEntity
+                        .created(URI.create("api/productos/".concat(p.getId())))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(p));
     }
 
     @PutMapping("{id}")
